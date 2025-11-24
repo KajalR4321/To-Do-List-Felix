d8ad27262e3e5b207a97a7e8f8fc757ecb77a392
Kajal Rajak <rajakkajal219@gmail.com>
2025-11-24 12:48:58 +0530

    add local storage of login page

--- JSFolder/App.js
+++ JSFolder/App.js
@@ -7,4 +7,50 @@ document.addEventListener("DOMContentLoaded", () => {
     window.location.href = "login.html";
     return;
   }
+// Get the list of all registered users
+  let userList =
+    JSON.parse(localStorage.getItem("userList")) || [];
+    
+// Get the email of the currently logged-in user
+
+  let user_email = localStorage.getItem("user_email") || "";
+
+// Find the matching user object using the email
+
+  let user_data = userList.find(
+    (user) => user.email === user_email
+  );
+
+  //  displaying the user name
+  document.getElementById("user_name").innerText =
+    user_data.name || "user";
+
+  //  modal logic
+  const add_form_modal = document.getElementById(
+    "add_form_modal"
+  );
+  const add_todo_btn =
+    document.getElementById("add_todo_btn");
+  const close_btn = document.getElementById("close_btn");
+//use for model or pop out
+  add_todo_btn.addEventListener("click", () => {
+    add_form_modal.showModal();
+  });
+  close_btn.addEventListener("click", () => {
+    add_form_modal.close();
+  });
+
+  //  showing todo list
+
+  const todo_container = document.getElementById(
+    "todo_container"
+  );
+
+  if (user_data.todo.length > 0) {
+    //  if data avaliable map it in html
+  }// If no tasks available → show message
+  else {
+    todo_container.innerHTML =
+      "<p> No Task available, Add task to see one </p>";
+  }
 });
---
+++ JSFolder/login.js
@@ -0,0 +1,43 @@
+// this event run the whole html page is loaded completely
+document.addEventListener("DOMContentLoaded",()=>{
+   
+    //selecting the form tag from html
+    const from =document.querySelector("form");
+
+    //Adding submit button event to the form
+
+    from.addEventListener("submit", (e)=>
+    {
+       e.preventDefault();
+     // This stops form from refreshing the page
+
+    // Getting email value from input field
+    const email = document.getElementById(email).value.trim().toLowerCase()
+ // trim() removes spaces
+      // toLowerCase() makes email lowercase for matching
+
+      const password=document.getElementById(password).value.trim().toLowerCase()
+
+      // Getting all users stored in localStorage
+    // If no user found → return empty array []
+    let userList= JSON.parse(localStorage.getItem("userList"))||[];
+
+        // Checking if email + password exists in stored users
+        let userExit= userList.find(
+            (user) => user.email===email && user.password === password
+        );
+        console.log(userExit);
+        // If user not found → show error message
+    if (!userExit) {
+      alert("Username and password do not match, Try Again!");
+      return;
+    }
+     // If user found → saving login details
+    localStorage.setItem("user_email", userExit.email);
+    localStorage.setItem("name", userExit.name);
+    localStorage.setItem("loggedIn", true);
+
+    // Redirect to homepage
+    window.location.href = "index.html";
+    })
+});
